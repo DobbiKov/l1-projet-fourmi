fa2d258f2e1dea33b4afd04063664c26cfe7a6e4
DobbiKov <dobbikov@gmail.com>
2024-04-24 15:23:35 +0200

    finished grille.hpp and grille.cpp. finished all the demanded functions from TD in these two files

--- include/projet_fourmi/grille.hpp
+++ include/projet_fourmi/grille.hpp
@@ -17,13 +17,24 @@ public:
     void changePlace(Place p);
     void decreasePheroSugar();
     int getCoordIdx(const Coord& c) const;
+    Place loadPlaceById(int id) const;
     vector<Place> getPlaces() const {return places;}
+    //la taille du tableau places
+    int numOfPlaces() const {return places.size(); }
 };
 
-//TODO: 
 void setNid(Grille& g, EnsCoord ens);
+void setSugar(Grille& g, EnsCoord ens);
 void setFourmis(Grille& g, vector<Fourmi> fourmis);
 Grille initializeGrille(vector<Fourmi> fourmis, EnsCoord sugar_coords, EnsCoord nid_coords);
 void linearizePheroNid(Grille& g);
 void decreasePheroSugar(Grille& g);
+
+void areFourmiGrilleCoherent(const Grille &g, const Fourmi &f);
+
+// returns the coordinates of places around the nid (can be used for the coordinates of fourmis)
+EnsCoord coordsAroundNid(EnsCoord ens_nid);
+
+// returns the biggest intensity of the pheromons of the place among the neigbours
+float getMaximalNeighbourPheroNid(Grille& g, Place p);
 #endif
--- src/grille.cpp
+++ src/grille.cpp
@@ -30,9 +30,103 @@ int Grille::getCoordIdx(const Coord& c) const{
     return idx;
 }
 
+Place Grille::loadPlaceById(int id) const{
+    if(id >= places.size() || id < 0) 
+        throw invalid_argument("The id is out of range!");
+    return places[id];
+}
+
 void Grille::decreasePheroSugar(){
     for(int i = 0; i < places.size(); i++){
         places[i].decreasePheroSugar();
     }
+}
+
+void setNid(Grille &g, EnsCoord ens){
+    for(int i = 0; i < ens.getCoords().size(); i++){
+        Coord c = ens.ieme(i);
+        Place p = g.loadPlace(c);
+        p.setNid();
+        p.setPheroNid(1);
+        g.changePlace(p);
+    }
+}
+
+void setSugar(Grille& g, EnsCoord ens){
+    for(int i = 0; i < ens.getCoords().size(); i++){
+        Coord c = ens.ieme(i);
+        Place p = g.loadPlace(c);
+        p.setSugar();
+        g.changePlace(p);
+    }
+}
+
+void setFourmis(Grille& g, vector<Fourmi> fourmis){
+    for(int i = 0; i < fourmis.size(); i++){
+        Fourmi f = fourmis[i];
+        Coord c = f.getCoords();
+        Place p = g.loadPlace(c);
+        p.setFourmi(f);
+        g.changePlace(p);
+    }
+}
+
+
+
+float getMaximalNeighbourPheroNid(Grille& g, Place p){
+    EnsCoord voisins = voisines(p.getCoords());
+    float max = 0;
+    for(Coord c: voisins.getCoords()){
+        Place p = g.loadPlace(c);
+        if(p.getPheroNid() > max)
+            max = p.getPheroNid();
+    }
+    return max;
+}
+
+void linearizePheroNid(Grille& g){
+    for(int i = 0; i < TAILLEGRILLE; i++){
+        for(int i = 0; i < g.numOfPlaces(); i++){
+            Place p = g.loadPlaceById(i);
+            if(p.getPheroNid() >= 1) continue;
+
+            float max_vois = getMaximalNeighbourPheroNid(g, p);
+            float new_intensity = max_vois - (1/TAILLEGRILLE);
+            p.setPheroNid( 
+                new_intensity > 0 ? new_intensity : 0
+            );
+            g.changePlace(p);
+        }
+    }
+}
+
+Grille initializeGrille(vector<Fourmi> fourmis, EnsCoord sugar_coords, EnsCoord nid_coords){
+    Grille g = Grille();
+    setNid(g, nid_coords);
+    setSugar(g, sugar_coords);
+    setFourmis(g, fourmis);
+
+    linearizePheroNid(g);
+    return g;
+}
+
+EnsCoord coordsAroundNid(EnsCoord ens_nid){
+    EnsCoord res = EnsCoord();
+    for(Coord c: ens_nid.getCoords()){
+        EnsCoord voisins = voisines(c);
+        for(Coord v: voisins.getCoords()){
+            if(res.contient(v) || ens_nid.contient(v)) continue;
+            res.ajoute(v);
+        }
+    }
+    return res;
+}
+
+void areFourmiGrilleCoherent(const Grille &g, const Fourmi &f){
+    Coord c = f.getCoords();
+    Place p = g.loadPlace(c);
+
+    if(p.getFourmiID() != f.getNum()) 
+        throw runtime_error("The state of the program isn't coherent! Error: Grille and Fourmi");
 }
 
